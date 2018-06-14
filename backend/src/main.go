cb9b1b17937918d86141c80b6a3a74de2df9384d
mayue42 <mayue@unity3D.com>
2018-06-14 16:35:41 +0800

    add upload to s3 basic feature

--- backend/src/main.go
+++ backend/src/main.go
@@ -6,40 +6,19 @@ import (
 	"net/http"
 	"log"
 	"os"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
 )
 
-// 获取大小的借口
-type Sizer interface {
-	Size() int64
-}
 
-// hello world, the web server
-func HelloServer(w http.ResponseWriter, r *http.Request) {
-	if "POST" == r.Method {
-		w.Header().Add("Content-Type", "text/html")
-		w.Header().Add("Access-Control-Allow-Origin","*")
-		file, _, err := r.FormFile("file")
-		if err != nil {
-			http.Error(w, err.Error(), 500)
-			return
-		}
-		defer file.Close()
-		f,err:=os.Create("filenametosaveas")
-		defer f.Close()
-		x,err:=io.Copy(f,file)
-		if err!=nil {
-			http.Error(w, err.Error(), 500)
-		}
-		fmt.Fprintf(w, "上传文件的大小为: %d", x)
-		return
-	}
 
-	// 上传页面
+func Index(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "text/html")
 	w.Header().Add("Access-Control-Allow-Origin","*")
 	w.WriteHeader(200)
 	html := `
-		<form enctype="multipart/form-data" action="/hello" method="POST">
+		<form enctype="multipart/form-data" action="/upload" method="POST">
 			Send this file: <input name="file" type="file" />
 			<input type="submit" value="Send File" />
 		</form>`
@@ -47,8 +26,70 @@ func HelloServer(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, html)
 }
 
+
+func UploadLocal(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/html")
+	w.Header().Add("Access-Control-Allow-Origin","*")
+	if "POST" == r.Method {
+		file, file_header, err := r.FormFile("file")
+		if err != nil {
+			http.Error(w, err.Error(), 500)
+			return
+		}
+		defer file.Close()
+		filename := file_header.Filename
+		f, err := os.Create("./upload/" + filename)
+		if err != nil {
+			http.Error(w, err.Error(), 500)
+			return
+		}
+		defer f.Close()
+		x, err := io.Copy(f, file)
+		if err != nil {
+			http.Error(w, err.Error(), 500)
+			return
+		}
+		fmt.Fprintf(w, "上传文件的大小为: %d", x)
+		return
+	}
+}
+
+
+func UploadS3(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/html")
+	w.Header().Add("Access-Control-Allow-Origin","*")
+	if "POST" == r.Method {
+		file, file_header, err := r.FormFile("file")
+		if err != nil {
+			http.Error(w, err.Error(), 500)
+			return
+		}
+		filename := file_header.Filename
+		defer file.Close()
+		sess := session.Must(session.NewSession(&aws.Config{Region: aws.String("us-east-1")}))
+		uploader := s3manager.NewUploader(sess)
+		bucket := "paradox42"
+		_, err = uploader.Upload(&s3manager.UploadInput{
+			Bucket: aws.String(bucket),
+			Key:    aws.String(filename),
+			Body:   file,
+		})
+		if err != nil {
+			// Print the error and exit.
+			http.Error(w, fmt.Sprintf("Unable to upload %q to %q, %v", filename, bucket, err), 500)
+			return
+		}
+
+		fmt.Fprintf(w, "Successfully uploaded %q to %q\n", filename, bucket)
+		return
+	}
+}
+
+
 func main() {
-	http.HandleFunc("/hello", HelloServer)
+	http.HandleFunc("/",Index)
+	http.HandleFunc("/upload", UploadLocal)
+	http.HandleFunc("/uploadtos3", UploadS3)
 	fmt.Println("listen on port 8080")
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
